e21199453a409e575e4428e7179c448f373327df
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-05-24 15:25:29 +0300

    class Orange, passGrowingSeason

--- orange-tree.js
+++ orange-tree.js
@@ -1,16 +1,23 @@
 class OrangeTree {
+  constructor(age, height, orange) {
+    this.age = age;
+    this.height = height;
+    this.orange = orange;
+  }
 
   passGrowingSeason() {
-
+    this.age += 1;
+    this.orange = 0;
+    while(this.height < 25) this.height += 2.5;
+    if (this.age > 6)  this.orange = Math.floor(Math.random()* (300-100) + 100)
   }
 
   isMature() {
-    //  Returns true if the tree is old enough to bear fruit, false otherwise
-  }
+// Возвращает true, если дерево достаточно взрослое, чтобы приносить плоды, иначе false }  }
 
   hasOranges() {
-    //  Returns true if there are any oranges on the tree, false otherwise
-  }
+// Возвращает true, если на дереве есть апельсины, иначе false
+}
 
   pickAnOrange() {
     //  Returns an Orange if there are any
@@ -20,6 +27,6 @@ class OrangeTree {
     }
     //  orange-picking logic goes here
   }
-}
 
+  }
 module.exports = OrangeTree;
